71f1845513d692701de7b297ff983cbee298decb
Mahdi <mahdinobar1993@gmail.com>
2018-12-19 19:57:59 +0100

    Version 6

---
+++ V6/Mini-project4-18-12-with-analyze/Mini-project4-18-12-with-analyze/three_link_biped_assign4_start/control/desired_outputs.m
@@ -0,0 +1,11 @@
+% If you are using the virtual constraints or trajectory based control
+% methods, then you may cosider using a function such as this. 
+function [hd, dhd] = desired_outputs(s, ds, q0, dq0, step_number)
+%takes in current position and velocity, start position and velocity of
+%step, and step number
+
+%outputs desired position and velocity to track for q2
+
+
+
+end
--- V6/Mini-project4-18-12-with-analyze/Mini-project4-18-12-with-analyze/three_link_biped_assign4_start/solve_eqns/eqns.m
+++ V6/Mini-project4-18-12-with-analyze/Mini-project4-18-12-with-analyze/three_link_biped_assign4_start/solve_eqns/eqns.m
@@ -1,11 +1,13 @@
 function dy = eqns(t, y, y0, step_number)
 % n this is the dimension of the ODE, note that n is 2*DOF, why? 
 % y1 = q1, y2 = q2, y3 = q3, y4 = dq1, y5 = dq2, y6 = dq3
+% trying to add y7 = u1, y8 = u2
 % y0 is the states right after impact
 
 q = [y(1); y(2); y(3)];
 dq = [y(4); y(5); y(6)];
-
+u_prior = [y(7);y(8)];
+    
 q0 = [y0(1); y0(2); y0(3)];
 dq0 = [y0(4); y0(5); y0(6)];
 
@@ -14,14 +16,25 @@ C = eval_C(q, dq);
 G = eval_G(q);
 B = eval_B();
 
-u = control(t, q, dq, q0, dq0, step_number); % for the moment we set the control outputs to zero
+u = control(t, q, dq, q0, dq0, step_number); 
+global u_vs_time1 u_vs_time2 u_vs_timet
+u_vs_time1=[u_vs_time1;u(1)];
+u_vs_time2=[u_vs_time2;u(2)];
+u_vs_timet=[u_vs_timet;t];
+
 u_ext = perturbation(q, step_number); % perturbation torque due to the external force at hip
 
-n = 6;   
+n = 8;   %trying to add u as columns 7 and 8 of output of ode45
 dy = zeros(n, 1);
 dy(1) = y(4);
 dy(2) = y(5);
 dy(3) = y(6);
-dy(4:6) = M \ (-C*dq - G + B*(u + u_ext));
+dy(4:6) = M \ (-C*dq - G + B*(u+u_ext));
+dy(7) = (u(1) - u_prior(1));
+dy(8) = (u(2) - u_prior(2));
+
+
+global input_with_step_number
+input_with_step_number(step_number,:)=u;
 
 end
